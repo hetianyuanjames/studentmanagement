01ae978084eef7b98475ef92e9e2d6b741afa0c2
hetianyuanjames <hty1016@ad.unc.edu>
2022-08-07 12:33:37 -0400

    Initial commit

---
+++ employeemanager/src/main/java/com/hetianyuanjames/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,11 @@
+package com.hetianyuanjames.employeemanager.repo;
+
+import com.hetianyuanjames.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
+}
